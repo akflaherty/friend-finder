af68f16daf9482e6e2e5f6076ee81e51041992ab
Annie Flaherty <anne.flaherty@case.edu>
2018-01-31 12:29:26 -0500

    Added script to collect radio button data

--- server.js
+++ server.js
@@ -16,10 +16,49 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
+app.get("/survey.html", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
 
 
+app.get("/api/friends", function(req, res) {
+  res.json(friendsArr);
+});
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+// !!! HARD CODED TEST DATA
+var friendsArr = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+}, {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+    ]
+}];
