9228950995d469c733667a675be2f8713b7d98f4
Annie Flaherty <anne.flaherty@case.edu>
2018-01-31 14:00:04 -0500

    Added API post function

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 app.use(bodyParser.json());
 
@@ -22,7 +22,7 @@ app.get("/survey.html", function(req, res) {
 
 
 app.get("/api/friends", function(req, res) {
-  res.json(friendsArr);
+    res.json(friendsArr);
 });
 
 // Starts the server to begin listening
@@ -30,35 +30,20 @@ app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
 
+app.post("/api/new", function(req, res) {
+    var newFriend = req.body;
+    console.log(newFriend);
+    friendsArr.push(newFriend);
+    res.json(newFriend);
+});
+
 // !!! HARD CODED TEST DATA
 var friendsArr = [{
     "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-    ]
+    "photo": "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/153558006-tips-healthy-cat-632x475.jpg",
+    "scores": ["5","1","4","4","5","1","2","5","4","1"]
 }, {
     "name": "Louis T. Delia",
     "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    "scores": [
-        "3",
-        "3",
-        "4",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2",
-        "2",
-        "3"
-    ]
+    "scores": ["3","3","4","2","2","1","3","2","2","3"]
 }];
